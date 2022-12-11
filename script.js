315dbe6b7e9f70df040da99e8d14318a7fded5f4
ggpukar <112745414+ggpukar@users.noreply.github.com>
2022-12-11 22:00:30 +0545

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-setInterval(function() {
-  var date = new Date();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var seconds = date.getSeconds();
-  var time = hours + ":" + minutes + ":" + seconds;
-  document.getElementById("time").innerHTML = time;
-}, 1000);
