22570bff79396bbcbae15facab7285a34a667fc8
ggpukar <112745414+ggpukar@users.noreply.github.com>
2022-12-11 21:54:01 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var time = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("time").innerHTML = time;
+}, 1000);
